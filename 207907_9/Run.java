e6111f4616c080e10ff7649a47dce56b30bb4507
Amrit Pandey <amritpandey@tuta.io>
2022-04-23 23:50:07 +0530

    Decision Tree Algorithm complete

--- 207907_9/Run.java
+++ 207907_9/Run.java
@@ -11,85 +11,27 @@ public class Run {
             getAttributeValues(D, i);
         }
         boolean[] usedAttribute = new boolean[attributeList.length];
-        // usedAttribute[usedAttribute.length - 1] = true;
-        // for (String[] values : attributeValues) {
-        //     for (String v : values) {
-        //         System.out.print(v + " ");
-        //     }
-        //     System.out.println();
-        // }
+        System.out.println("DECISION TREE CLASSIFIER");
+        System.out.println("------------------------");
+        System.out.println();
         Node tree = GenerateDecisionTree(D, attributeList, usedAttribute, 0);
     }
 
-    public static String[] getAttributeList(String dbname) throws IOException {
-        File f = new File(dbname);
-        FileReader fr = new FileReader(f);
-        BufferedReader br = new BufferedReader(fr);
-        String line = br.readLine();
-        br.close();
-        return line.split(" ");
-    }
-
-    public static List<String> getDataFromFile(String dbname) throws IOException {
-        List<String> data = new ArrayList<>();
-        File f = new File(dbname);
-        FileReader fr = new FileReader(f);
-        BufferedReader br = new BufferedReader(fr);
-        String line = null;
-        int i = 0;
-
-        while ((line = br.readLine()) != null) {
-            if (i == 0) {
-                i++;
-                continue;
-            }
-            data.add(line);
-        }
-
-        br.close();
-        return data;
-    }
-
-    public static void getAttributeValues(List<String> D, int attribute) {
-        List<String> values = new ArrayList<>();
-        for (String row : D) {
-            String[] tuple = row.split(" ");
-            if (!values.contains(tuple[attribute])) {
-                values.add(tuple[attribute]);
-            }
-        }
-        String[] tmp = new String[values.size()];
-        String[] o = values.toArray(tmp);
-        attributeValues.add(o);
-    }
-
-    public static List<String[]> attributeValues = new ArrayList<>();
-
-    public static class Node {
-        String label;
-        List<Node> children;
-
-        public Node(String label, List<Node> children) {
-            this.label = label;
-            this.children = new ArrayList<>();
-        }
-
-        public Node() {
-            this.label = "";
-            this.children = new ArrayList<>();
-        }
-    }
-
+    // Implementation of standard Decision Tree Algorithm
+    /**
+     * @param Database
+     * @param attributeList
+     * @param usedAttribute
+     * @param level
+     */
     public static Node GenerateDecisionTree(List<String> D, String[] attributeList, boolean[] usedAttribute, int level) {
         Node node = new Node();
         if (hasSameClass(D)) {
-            // System.out.println("\t\thas same class");
             node.label = getClassOf(D);
             System.out.println("\t\tLeaf(" + node.label + ")");
             return node;
         }
         if (isEmpty(usedAttribute)) {
-            // System.out.println("\t\tall attribute used");
             node.label = getClassOf(D);
             System.out.println("\t\tLeaf(" + node.label + ")");
             return node;
@@ -120,6 +62,25 @@ public class Run {
         return node;
     }
 
+    // variable to collect attribute values
+    public static List<String[]> attributeValues = new ArrayList<>();
+
+    // structure of Node
+    public static class Node {
+        String label;
+        List<Node> children;
+
+        public Node(String label, List<Node> children) {
+            this.label = label;
+            this.children = new ArrayList<>();
+        }
+
+        public Node() {
+            this.label = "";
+            this.children = new ArrayList<>();
+        }
+    }
+
     public static boolean hasSameClass(List<String> D) {
         int L = D.get(0).split(" ").length - 1;
         String beginClass = D.get(0).split(" ")[L];
@@ -131,6 +92,29 @@ public class Run {
         }
         return true;
     }
+
+    public static String[] getAttributeList(String dbname) throws IOException {
+        File f = new File(dbname);
+        FileReader fr = new FileReader(f);
+        BufferedReader br = new BufferedReader(fr);
+        String line = br.readLine();
+        br.close();
+        return line.split(" ");
+    }
+
+    public static void getAttributeValues(List<String> D, int attribute) {
+        List<String> values = new ArrayList<>();
+        for (String row : D) {
+            String[] tuple = row.split(" ");
+            if (!values.contains(tuple[attribute])) {
+                values.add(tuple[attribute]);
+            }
+        }
+        String[] tmp = new String[values.size()];
+        String[] o = values.toArray(tmp);
+        attributeValues.add(o);
+    }
+
 
     public static boolean isEmpty(boolean[] usedAttributes) {
         for (boolean val : usedAttributes) {
@@ -180,20 +164,15 @@ public class Run {
                 maxGain = Gain(D, i, attribute_list);
             }
         }
-        // System.out.println("Splitting Attribute = " + attribute_list[chosenIndex]);
         usedAttributes[chosenIndex] = true;
         return chosenIndex;
     }
 
     public static double Gain(List<String> D, int attributeIndex, String[] attributeList) {
-        // System.out.println();
-        // System.out.println("Gain for " + attributeList[attributeIndex]);
         double Gain = 0;
         double InfoD = Info(D);
-        // System.out.println("Info(D) = " + InfoD);
 
         List<String> attributeValues = new ArrayList<>();
-        // collect attribute values
         for (String t : D) {
             String[] tuple = t.split(" ");
             if (!attributeValues.contains(tuple[attributeIndex])) {
@@ -213,18 +192,14 @@ public class Run {
             Gain += ((double) Dj.size() / D.size()) * Info(Dj);
         }
 
-        // System.out.println("Info(" + attributeList[attributeIndex] + ") =" + Gain);
-
         Gain = InfoD - Gain;
 
-        // System.out.println("Gain(" + attributeList[attributeIndex] + ") = " + Gain);
         return Gain;
     }
 
     public static double Info(List<String> D) {
         double entropy = 0;
         int totalTuples = D.size();
-        // System.out.println("|D| = " + totalTuples);
         List<String> classLabels = new ArrayList<>();
         for (String t : D) {
             String[] tuple = t.split(" ");
@@ -242,17 +217,33 @@ public class Run {
                     pi[i] += 1;
                 }
             }
-            // System.out.println("Label(" + label + ") = " + pi[i]);
         }
 
-        // System.out.println("Total tuples = " + totalTuples);
-
         for (int i = 0; i < pi.length; i++) {
-            // System.out.println(pi[i] + "/" + totalTuples + " * " + Math.log(pi[i]));
             pi[i] /= totalTuples;
             entropy += -(pi[i] * Math.log(pi[i]));
         }
 
         return entropy;
+    }
+
+    public static List<String> getDataFromFile(String dbname) throws IOException {
+        List<String> data = new ArrayList<>();
+        File f = new File(dbname);
+        FileReader fr = new FileReader(f);
+        BufferedReader br = new BufferedReader(fr);
+        String line = null;
+        int i = 0;
+
+        while ((line = br.readLine()) != null) {
+            if (i == 0) {
+                i++;
+                continue;
+            }
+            data.add(line);
+        }
+
+        br.close();
+        return data;
     }
 }
